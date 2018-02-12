e55c26d988105e4fdd4f9da4b2546f2d7fa45d48
bonbondon <jdkaufman1@smcm.edu>
2018-02-11 23:49:43 -0500

    General Update 1

--- Instant Messager/src/Server.java
+++ Instant Messager/src/Server.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
 
 public class Server implements java.lang.Runnable{
 	private java.net.ServerSocket server_socket;
@@ -8,14 +11,32 @@ public class Server implements java.lang.Runnable{
 	Server(int port){
 		try {
 			server_socket = new ServerSocket(port);
+			System.out.println("Online.");
 		} catch (IOException e) {
 			System.out.println("Server socket cannot be opened.");
 		}//end try/catch
 	}//end constructor
 
 	public void run() {
-		while(true){
-			
-		}//end while
+		LinkedList<Socket> clientSockets = new LinkedList<Socket>();
+		Socket test;
+		Thread testThread;
+		LinkedList<Thread> threadList = new LinkedList<Thread>();
+//		PrintStream output;
+//		PrintStream in
+		try{
+			while(true){
+				test = server_socket.accept();
+				testThread = new Thread(new Connection(test, user_database));
+				testThread.start();
+				threadList.add(testThread);
+//				output = new PrintStream(test.getOutputStream());
+//				output.println("Welcome");
+				clientSockets.add(test);
+//				output.println("How are you?");
+			}//end while
+		}catch(IOException e){
+			System.out.println("darn");
+		}//end try/catch
 	}//end run
 }//end class
