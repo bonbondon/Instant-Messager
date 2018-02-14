972a5a2b0b9c8b1e13a5ff98e502d9976f85841f
bonbondon <jdkaufman1@smcm.edu>
2018-02-14 15:00:30 -0500

    Update 2

--- Instant Messager/src/Connection.java
+++ Instant Messager/src/Connection.java
@@ -48,10 +48,11 @@ public class Connection implements java.lang.Runnable{
 	}
 	public void run() {
 		output.println("Welcome.");
-		output.println("Please enter your command..");
-		//SPECIAL
-		String commandLine = input.next();
-		String command = commandLine.substring(0, 4);
+		output.println("Please enter your command...");
+		String commandLine = input.nextLine();
+		commandLine = "";
+		String command = "";
+		//String command = commandLine.substring(0, 4);
 		if(command.equals("CRTE")){
 			
 		}else if(command.equals("AUTH")){
